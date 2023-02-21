944b6257313f1d5eb73fe92ae52a477cffac6583
Abdurasul <abdurasulabduraimov22@gmail.com>
2023-02-22 01:29:14 +0500

    initial commit

---
+++ src/main/java/com/example/universitystartup/UniversityStartUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.universitystartup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityStartUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityStartUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/universitystartup/UniversityStartUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.universitystartup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityStartUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
